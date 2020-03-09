1318b1e8b1e95856e2fd817f1a1b7519e0e5b066
Kelby Mittan <kelbymittan@pursuit.org>
2020-03-08 21:25:59 -0400

    figured out roman to int

--- 02-19-2020-DSA.playground/Contents.swift
+++ 02-19-2020-DSA.playground/Contents.swift
@@ -293,26 +293,33 @@ shiftZeros([3,0,2,0,0,1,0,4])
 print(shiftZeros([3,0,2,0,0,1,0,4]))
 
 func romanToInt(_ str: String) -> Int {
-    var res = 0
+    var result = 0
     let dict: [Character : Int] =
         ["I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000]
-    
+
     var strArr = [Character]()
-    
+
     for char in str {
         strArr.append(char)
-        strArr = strArr.reversed()
     }
     
+    var prev = 0
+
     for i in 0..<strArr.count {
         
-        if i - 1 >= 0 , dict[strArr[i]]! > dict[strArr[i - 1]]! {
-            res += dict[strArr[i]]! - (2 * dict[strArr[i - 1]]!)
+        print(prev)
+        print(dict[strArr[i]]!)
+        print(result)
+        print(strArr)
+        if prev >= dict[strArr[i]]! {
+            result += dict[strArr[i]]!
         } else {
-            res += dict[strArr[i]]!
+            result = result - (2 * prev) + dict[strArr[i]]!
         }
+        prev = dict[strArr[i]]!
+        
     }
-    return res
+    return result
 }
 
-romanToInt("LX")
+romanToInt("IV")
