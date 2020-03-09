05e61a2b9412b1dcb17c819117f40820d40e93a7
Kelby Mittan <kelbymittan@pursuit.org>
2020-03-09 12:44:34 -0400

    updated

--- 02-19-2020-DSA.playground/Contents.swift
+++ 02-19-2020-DSA.playground/Contents.swift
@@ -292,34 +292,4 @@ shiftZeros([3,0,2,0,0,1,0,4])
 
 print(shiftZeros([3,0,2,0,0,1,0,4]))
 
-func romanToInt(_ str: String) -> Int {
-    var result = 0
-    let dict: [Character : Int] =
-        ["I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000]
 
-    var strArr = [Character]()
-
-    for char in str {
-        strArr.append(char)
-    }
-    
-    var prev = 0
-
-    for i in 0..<strArr.count {
-        
-        print(prev)
-        print(dict[strArr[i]]!)
-        print(result)
-        print(strArr)
-        if prev >= dict[strArr[i]]! {
-            result += dict[strArr[i]]!
-        } else {
-            result = result - (2 * prev) + dict[strArr[i]]!
-        }
-        prev = dict[strArr[i]]!
-        
-    }
-    return result
-}
-
-romanToInt("IV")
