dea21fa955add635db9e83db279710658fdac577
Kelby Mittan <kelbymittan@pursuit.org>
2020-04-02 13:35:26 -0400

    completed self dividing numbers 728

--- LeetCodeQuestions.playground/Pages/April-2-2020.xcplaygroundpage/Contents.swift
+++ LeetCodeQuestions.playground/Pages/April-2-2020.xcplaygroundpage/Contents.swift
@@ -81,3 +81,55 @@ func countSmaller(_ nums: [Int]) -> [Int] {
     return resultArr
 }
 countSmaller([5,2,6,1])
+
+/*
+ 728. Self Dividing Numbers
+ Easy
+
+ A self-dividing number is a number that is divisible by every digit it contains.
+ For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+ Also, a self-dividing number is not allowed to contain the digit zero.
+
+ Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+ Example 1:
+ Input:
+ left = 1, right = 22
+ Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+ Note:
+
+ The boundaries of each input argument are 1 <= left <= right <= 10000.
+ */
+
+func selfDividingNumbers(_ left: Int, _ right: Int) -> [Int] {
+    var resultsArr = [Int]()
+    
+    for num in left...right {
+        let numArr = Array(String(num))
+        if !numArr.contains("0") {
+            for i in numArr {
+                let iInt = Int(String(i)) ?? 0
+                if num % iInt == 0 {
+                    if !resultsArr.contains(num) {
+                        resultsArr.append(num)
+                    }
+                    continue
+                } else {
+                    resultsArr.removeAll(where: { $0 == num })
+                    break
+                }
+            }
+        }
+    }
+    return resultsArr
+}
+
+selfDividingNumbers(1, 22)
+
+var str = 1230
+
+let arr = Array(String(str))
+
+for i in arr {
+    let iNum = Int(String(i))
+}
