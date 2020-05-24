cdb22a0fa5a92d5192a7aaa3cc33372c3672bcbc
Kelby Mittan <kelbymittan@pursuit.org>
2020-05-24 19:48:21 -0400

    mid node in linked list

---
+++ LeetCodeQuestions.playground/Pages/LinkedList-Design.xcplaygroundpage/Contents.swift
@@ -0,0 +1,186 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class MyLinkedList {
+    
+    class LinkedListNode {
+        let value: Int
+        var next: LinkedListNode?
+        init(value: Int, next: LinkedListNode? = nil) {
+            self.value = value
+            self.next = next
+        }
+    }
+    
+    var head: LinkedListNode?
+    
+    init() {
+        head = nil
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    func get(_ index: Int) -> Int {
+        guard let node = node(at: index) else {
+            return -1
+        }
+        return node.value
+    }
+    
+    private func node(at: Int) -> LinkedListNode? {
+        guard at > -1, var node = head else {
+            return nil
+        }
+        guard at > 0 else {
+            return head
+        }
+        for _ in 1...at {
+            guard let nextNode = node.next else {
+                return nil
+            }
+            node = nextNode
+        }
+        return node
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    func addAtHead(_ val: Int) {
+        let newNode = LinkedListNode(value: val, next: head)
+        head = newNode
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    func addAtTail(_ val: Int) {
+        guard head != nil else {
+            head = LinkedListNode(value: val)
+            return
+        }
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = LinkedListNode(value: val)
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    func addAtIndex(_ index: Int, _ val: Int) {
+        if index <= 0 {
+            addAtHead(val)
+            return
+        }
+        guard let prevNode = node(at: index - 1) else {
+            return
+        }
+        let newNode = LinkedListNode(value: val, next: prevNode.next)
+        prevNode.next = newNode
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    func deleteAtIndex(_ index: Int) {
+        if index == 0, head != nil {
+            head = head?.next
+            return
+        }
+        guard let prevNode = node(at: index - 1) else {
+            return
+        }
+        prevNode.next = prevNode.next?.next
+    }
+}
+
+extension MyLinkedList: CustomStringConvertible {
+    // 2
+    public var description: String {
+        // 3
+        var text = "["
+        var node = head
+        // 4
+        while node != nil {
+            text += "\(node!.value)"
+            node = node!.next
+            if node != nil { text += ", " }
+        }
+        // 5
+        return text + "]"
+    }
+}
+
+/*
+ 203. Remove Linked List Elements
+ Easy
+ 
+ Remove all elements from a linked list of integers that have value val.
+ 
+ Example:
+ 
+ Input:  1->2->6->3->4->5->6, val = 6
+ Output: 1->2->3->4->5
+ */
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+    
+    let fakeHead = ListNode(-1)
+    fakeHead.next = head
+    var prev = fakeHead
+    while let current = prev.next {
+        if current.val == val {
+            prev.next = current.next
+        } else {
+            prev = current
+        }
+    }
+    return fakeHead.next
+}
+
+
+/*
+ 876. Middle of the Linked List
+ Easy
+ 
+ Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ 
+ If there are two middle nodes, return the second middle node.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: [1,2,3,4,5]
+ Output: Node 3 from this list (Serialization: [3,4,5])
+ The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+ Note that we returned a ListNode object ans, such that:
+ ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+ Example 2:
+ 
+ Input: [1,2,3,4,5,6]
+ Output: Node 4 from this list (Serialization: [4,5,6])
+ Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
+
+func middleNode(_ head: ListNode?) -> ListNode? {
+    var count = 0
+    var midNode = head
+    
+    while midNode != nil {
+        count += 1
+        midNode = midNode?.next
+    }
+    midNode = head
+    
+    var midVal = count/2
+    
+    while midVal != 0 {
+        midVal -= 1
+        midNode = midNode?.next
+    }
+    
+    return midNode
+}
--- LeetCodeQuestions.playground/Pages/May-24-destinationCity.xcplaygroundpage/Contents.swift
+++ LeetCodeQuestions.playground/Pages/May-24-destinationCity.xcplaygroundpage/Contents.swift
@@ -88,5 +88,136 @@ func toLowerCase(_ str: String) -> String {
 }
 
 /*
- 
+ 151. Reverse Words in a String
+ Medium
+
+ Example 1:
+
+ Input: "the sky is blue"
+ Output: "blue is sky the"
+ Example 2:
+
+ Input: "  hello world!  "
+ Output: "world! hello"
+ Explanation: Your reversed string should not contain leading or trailing spaces.
+ Example 3:
+
+ Input: "a good   example"
+ Output: "example good a"
+ Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
  */
+
+func reverseWords(_ s: String) -> String {
+    let sArr = s.components(separatedBy: " ")
+    var resultArr = [String]()
+    for i in 0..<sArr.count {
+        let word = sArr[i]
+        
+        if word != "" {
+            resultArr.insert(word + " ", at: 0)
+        }
+    }
+    var result = resultArr.joined()
+    result.removeLast()
+    return result
+}
+
+reverseWords("  hello world!  ")
+
+/*
+ 1021. Remove Outermost Parentheses
+ Easy
+
+ A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+ A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+ Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+ Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+  
+
+ Example 1:
+
+ Input: "(()())(())"
+ Output: "()()()"
+ Explanation:
+ The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+ After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+ Example 2:
+
+ Input: "(()())(())(()(()))"
+ Output: "()()()()(())"
+ Explanation:
+ The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+ After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+ Example 3:
+
+ Input: "()()"
+ Output: ""
+ Explanation:
+ The input string is "()()", with primitive decomposition "()" + "()".
+ After removing outer parentheses of each part, this is "" + "" = "".
+ */
+
+func removeOuterParentheses(_ S: String) -> String {
+    var result = ""
+    var counter = 0
+    for s in S {
+        if s == "(" {
+           if counter > 0 {
+              result.append(s)
+           }
+           counter += 1
+        } else if s == ")" {
+           counter -= 1
+           if counter > 0 {
+               result.append(s)
+           }
+        }
+    }
+    return result
+}
+
+removeOuterParentheses("(()())(())(()(()))")
+
+/*
+ 657. Robot Return to Origin
+ Easy
+
+ There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+
+ The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+
+ Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+
+ Example 1:
+
+ Input: "UD"
+ Output: true
+ Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
+  
+
+ Example 2:
+
+ Input: "LL"
+ Output: false
+ Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
+ */
+
+func judgeCircle(_ moves: String) -> Bool {
+    
+    var dict = [Character:Int]()
+    
+    for move in moves {
+        if dict[move] == nil {
+            dict[move] = 1
+        } else {
+            dict[move]! += 1
+        }
+    }
+    return dict["L"] == dict["R"] && dict["U"] == dict["D"]
+}
+
+judgeCircle("UDU")
