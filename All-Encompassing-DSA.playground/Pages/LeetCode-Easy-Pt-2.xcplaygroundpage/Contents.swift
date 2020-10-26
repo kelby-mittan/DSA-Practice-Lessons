7cba98d8b71746719b16efff346c48cfbd6c40c3
Kelby Mittan <kelbymittan@pursuit.org>
2020-10-26 16:41:40 -0400

    lemonade and stolen robot answer

--- All-Encompassing-DSA.playground/Pages/LeetCode-Easy-Pt-2.xcplaygroundpage/Contents.swift
+++ All-Encompassing-DSA.playground/Pages/LeetCode-Easy-Pt-2.xcplaygroundpage/Contents.swift
@@ -52,6 +52,8 @@ func numberOfSteps (_ num: Int) -> Int {
     return numOfSteps
 }
 
+//=====================================================================================
+
 /*
  1221. Split a String in Balanced Strings
  Easy
@@ -101,4 +103,163 @@ func balancedStringSplit(_ s: String) -> Int {
     }
     
     return split
+}
+
+//=====================================================================================
+
+/*
+ 860. Lemonade Change
+ Easy
+ 
+ At a lemonade stand, each lemonade costs $5.
+ 
+ Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+ 
+ Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.  You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+ 
+ Note that you don't have any change in hand at first.
+ 
+ Return true if and only if you can provide every customer with correct change.
+ 
+ Example 1:
+ 
+ Input: [5,5,5,10,20]
+ Output: true
+ Explanation:
+ From the first 3 customers, we collect three $5 bills in order.
+ From the fourth customer, we collect a $10 bill and give back a $5.
+ From the fifth customer, we give a $10 bill and a $5 bill.
+ Since all customers got correct change, we output true.
+ Example 2:
+ 
+ Input: [5,5,10]
+ Output: true
+ Example 3:
+ 
+ Input: [10,10]
+ Output: false
+ Example 4:
+ 
+ Input: [5,5,10,10,20]
+ Output: false
+ Explanation:
+ From the first two customers in order, we collect two $5 bills.
+ For the next two customers in order, we collect a $10 bill and give back a $5 bill.
+ For the last customer, we can't give change of $15 back because we only have two $10 bills.
+ Since not every customer received correct change, the answer is false.
+ */
+
+func lemonadeChange(_ bills: [Int]) -> Bool {
+    var five = 0
+    var ten = 0
+    for i in bills {
+        if i == 5 {
+            five += 1
+        }
+        else if i == 10 {
+            five -= 1
+            ten += 1
+        }
+        else if i == 20 && ten > 0 {
+            ten -= 1
+            five -= 1
+        } else {
+            five -= 3
+        }
+        if five < 0 {
+            return false
+        }
+    }
+    return true
+}
+
+//=====================================================================================
+
+/*
+ 874. Walking Robot Simulation
+ Easy
+ 
+ A robot on an infinite grid starts at point (0, 0) and faces north.  The robot can receive one of three possible types of commands:
+ 
+ -2: turn left 90 degrees
+ -1: turn right 90 degrees
+ 1 <= x <= 9: move forward x units
+ Some of the grid squares are obstacles.
+ 
+ The i-th obstacle is at grid point (obstacles[i][0], obstacles[i][1])
+ 
+ If the robot would try to move onto them, the robot stays on the previous grid square instead (but still continues following the rest of the route.)
+ 
+ Return the square of the maximum Euclidean distance that the robot will be from the origin.
+ 
+ Example 1:
+ 
+ Input: commands = [4,-1,3], obstacles = []
+ Output: 25
+ Explanation: robot will go to (3, 4)
+ Example 2:
+ 
+ Input: commands = [4,-1,4,-2,4], obstacles = [[2,4]]
+ Output: 65
+ Explanation: robot will be stuck at (1, 4) before turning left and going to (1, 8)
+ */
+
+let multiplier: Int = 1000000
+enum Direction {
+    case north, south, west, east
+    func turnLeft() -> Direction {
+        switch self {
+        case .north: return .west
+        case .south: return .east
+        case .west: return .south
+        case .east: return .north
+        }
+    }
+    func turnRight() -> Direction {
+        switch self {
+        case .north: return .east
+        case .south: return .west
+        case .west: return .north
+        case .east: return .south
+        }
+    }
+    func dx() -> Int {
+        switch self {
+        case .east: return 1
+        case .west: return -1
+        default: return 0
+        }
+    }
+    func dy() -> Int {
+        switch self {
+        case .north: return 1
+        case .south: return -1
+        default: return 0
+        }
+    }
+}
+func zip(_ y: Int, _ x: Int) -> Int { return multiplier * (multiplier / 2 + y) + multiplier / 2 + x } // zip point to Int
+func robotSim(_ commands: [Int], _ obstacles: [[Int]]) -> Int {
+    var obstaclesDict: [Int: Bool] = [:]
+    for obstacle in obstacles { obstaclesDict[zip(obstacle[1], obstacle[0])] = true } // convert to dictionary
+    var curX: Int = 0
+    var curY: Int = 0
+    var curDirection: Direction = .north
+    var result: Int = 0
+    for command in commands {
+        switch command {
+        case -1:
+            curDirection = curDirection.turnRight()
+        case -2:
+            curDirection = curDirection.turnLeft()
+        default:
+            for _ in 1...command {
+                if obstaclesDict[zip(curY + curDirection.dy(), curX + curDirection.dx()), default: false] { break }
+                curX += curDirection.dx()
+                curY += curDirection.dy()
+                result = max(result, curX * curX + curY * curY)
+            }
+        }
+    }
+    return result
 }
