9d42753c914c7e9715b4ddc602995956a07c0386
Kelby Mittan <kelbymittan@pursuit.org>
2020-10-27 14:11:48 -0400

    min distance, max num

--- All-Encompassing-DSA.playground/Pages/LeetCode-Easy-Pt-2.xcplaygroundpage/Contents.swift
+++ All-Encompassing-DSA.playground/Pages/LeetCode-Easy-Pt-2.xcplaygroundpage/Contents.swift
@@ -263,3 +263,91 @@ func robotSim(_ commands: [Int], _ obstacles: [[Int]]) -> Int {
     }
     return result
 }
+
+/*
+ 1266. Minimum Time Visiting All Points
+ Easy
+
+ 550
+
+ 94
+
+ Add to List
+
+ Share
+ On a plane there are n points with integer coordinates points[i] = [xi, yi]. Your task is to find the minimum time in seconds to visit all points.
+
+ You can move according to the next rules:
+
+ In one second always you can either move vertically, horizontally by one unit or diagonally (it means to move one unit vertically and one unit horizontally in one second).
+ You have to visit the points in the same order as they appear in the array.
+  
+
+ Example 1:
+
+ Input: points = [[1,1],[3,4],[-1,0]]
+ Output: 7
+ Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]
+ Time from [1,1] to [3,4] = 3 seconds
+ Time from [3,4] to [-1,0] = 4 seconds
+ Total time = 7 seconds
+ 
+ Example 2:
+
+ Input: points = [[3,2],[-2,2]]
+ Output: 5
+ */
+
+func minTimeToVisitAllPoints(_ points: [[Int]]) -> Int {
+    var seconds = 0
+    for i in 0..<points.count - 1 {
+        let x = points[i+1][0]-points[i][0]
+        let y = points[i+1][1]-points[i][1]
+        seconds += max(abs(x), abs(y))
+    }
+    return seconds
+}
+
+
+/*
+ 1323. Maximum 69 Number
+ Easy
+
+ Given a positive integer num consisting only of digits 6 and 9.
+
+ Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+ Example 1:
+
+ Input: num = 9669
+ Output: 9969
+ Explanation:
+ Changing the first digit results in 6669.
+ Changing the second digit results in 9969.
+ Changing the third digit results in 9699.
+ Changing the fourth digit results in 9666.
+ The maximum number is 9969.
+ Example 2:
+
+ Input: num = 9996
+ Output: 9999
+ Explanation: Changing the last digit 6 to 9 results in the maximum number.
+ Example 3:
+
+ Input: num = 9999
+ Output: 9999
+ Explanation: It is better not to apply any change.
+ */
+
+func maximum69Number (_ num: Int) -> Int {
+    var numArr = Array(String(num))
+
+    for (i,n) in numArr.enumerated() {
+        if n == "6" {
+            numArr[i] = "9"
+            break
+        }
+    }
+
+    return Int(String(numArr))!
+}
