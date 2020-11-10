0376de6957e85582a3f7e4fdeb55872607e7ace9
Kelby Mittan <kelbymittan@pursuit.org>
2020-11-10 14:22:42 -0500

    repeat n times

--- Qs-from-LeetCode.playground/Pages/LeetCode-Easy-Pt.3.xcplaygroundpage/Contents.swift
+++ Qs-from-LeetCode.playground/Pages/LeetCode-Easy-Pt.3.xcplaygroundpage/Contents.swift
@@ -148,3 +148,44 @@ func removeOuterParentheses(_ S: String) -> String {
 }
 
 removeOuterParentheses("(()())(())(()(()))")
+
+//==================================================================================
+
+/*
+ 961. N-Repeated Element in Size 2N Array
+ Easy
+
+ In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+ Return the element repeated N times.
+
+ Example 1:
+
+ Input: [1,2,3,3]
+ Output: 3
+ Example 2:
+
+ Input: [2,1,2,5,3,2]
+ Output: 2
+ Example 3:
+
+ Input: [5,1,5,2,5,3,5,4]
+ Output: 5
+ */
+
+func repeatedNTimes(_ A: [Int]) -> Int {
+    var dict = [Int:Int]()
+
+    for n in A {
+        dict[n,default: 0] += 1
+    }
+    for (key,val) in dict where val == A.count/2 {
+        return key
+    }
+    return 0
+}
+
+repeatedNTimes([5,1,5,2,5,3,5,4])
+
+//==================================================================================
+
