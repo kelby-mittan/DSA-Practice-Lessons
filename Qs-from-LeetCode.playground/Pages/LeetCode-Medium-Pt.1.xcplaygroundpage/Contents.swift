eeda320dc472b8e64b64e1d4fae86df576f879fe
Kelby Mittan <kelbymittan@pursuit.org>
2020-11-17 18:10:11 -0500

    not sure how this qualifies as medium

--- Qs-from-LeetCode.playground/Pages/LeetCode-Medium-Pt.1.xcplaygroundpage/Contents.swift
+++ Qs-from-LeetCode.playground/Pages/LeetCode-Medium-Pt.1.xcplaygroundpage/Contents.swift
@@ -118,6 +118,40 @@ func sumEvenGrandparent(_ root: TreeNode?) -> Int {
 
 let rootNode = TreeNode(6, TreeNode(7, TreeNode(2, TreeNode(9, nil, nil), nil), TreeNode(7, TreeNode(1, nil, nil), TreeNode(4, nil, nil))), TreeNode(8, TreeNode(1, nil, nil), TreeNode(3, nil, TreeNode(5, nil, nil))))
 
-
-
 sumEvenGrandparent(rootNode)
+
+//================================================================================
+
+/*
+ 442. Find All Duplicates in an Array
+ Medium
+
+ Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+ Find all the elements that appear twice in this array.
+
+ Could you do it without extra space and in O(n) runtime?
+
+ Example:
+ Input:
+ [4,3,2,7,8,2,3,1]
+
+ Output:
+ [2,3]
+ */
+
+func findDuplicates(_ nums: [Int]) -> [Int] {
+
+    var occDict = [Int:Int]()
+    var resultArr = [Int]()
+    for num in nums {
+        if occDict[num] == nil {
+            occDict[num] = 1
+        } else {
+            resultArr.append(num)
+        }
+    }
+    return resultArr
+}
+
+findDuplicates([4,3,2,7,8,2,3,1])
