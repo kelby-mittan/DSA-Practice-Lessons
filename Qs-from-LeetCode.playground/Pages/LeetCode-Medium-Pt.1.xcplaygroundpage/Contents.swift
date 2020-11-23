84915de6bdde710420acaf49c8700934b4d91064
Kelby Mittan <kelbymittan@pursuit.org>
2020-11-23 17:57:13 -0500

    discussion

--- Qs-from-LeetCode.playground/Pages/LeetCode-Medium-Pt.1.xcplaygroundpage/Contents.swift
+++ Qs-from-LeetCode.playground/Pages/LeetCode-Medium-Pt.1.xcplaygroundpage/Contents.swift
@@ -330,3 +330,44 @@ func compress(_ chars: inout [Character]) -> Int {
 }
 var arrToCompress: [Character] = ["a","a","a","b","b","a","a"]
 compress(&arrToCompress)
+
+//===============================================================================
+
+/*
+ 189. Rotate Array
+ Medium
+
+ Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ Follow up:
+
+ Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ Could you do it in-place with O(1) extra space?
+  
+
+ Example 1:
+
+ Input: nums = [1,2,3,4,5,6,7], k = 3
+ Output: [5,6,7,1,2,3,4]
+ Explanation:
+ rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ Example 2:
+
+ Input: nums = [-1,-100,3,99], k = 2
+ Output: [3,99,-1,-100]
+ Explanation:
+ rotate 1 steps to the right: [99,-1,-100,3]
+ rotate 2 steps to the right: [3,99,-1,-100]
+ */
+
+func rotate(_ nums: inout [Int], _ k: Int) {
+        
+   let times = k % nums.count
+
+    nums = Array(nums[nums.count-times..<nums.count]) + Array(nums[0..<nums.count-times])
+}
+
+var rotateArr = [1,2,3,4,5,6,7]
+rotate(&rotateArr, 3)
